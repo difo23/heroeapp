5f010f1a31a608aafa5e7ee55086e9a305ac9c38
difo23 <diffozanzan@gmail.com>
2020-10-23 15:02:24 -0400

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroesApp from '../src/components/heroes-components/HeroesApp';
+
+function App() {
+	return (
+		<div className="App">
+			<HeroesApp />
+		</div>
+	);
+}
+
+export default App;
